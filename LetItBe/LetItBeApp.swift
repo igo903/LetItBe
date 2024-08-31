48fb4fae4d2d887d2914150f126b0eae377af3f6
Alain <31738345@qq.com>
2024-08-31 22:28:07 +0800

    Initial Commit

---
+++ LetItBe/LetItBeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetItBeApp.swift
+//  LetItBe
+//
+//  Created by alain on 2024/8/31.
+//
+
+import SwiftUI
+
+@main
+struct LetItBeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
